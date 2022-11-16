7cdd5cbb671c5bd0ac199cab9199fe5b1b060143
nickync <lizheng9024@gmail.com>
2022-11-15 22:38:12 -0500

    day 1

---
+++ pt/src/main/java/Model/Trainer.java
@@ -0,0 +1,112 @@
+package Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Trainer")
+public class Trainer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "years_of_exp")
+    private int yearsOfExperience;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "background")
+    private String background;
+
+    public Trainer() {
+
+    }
+
+    public Trainer(Long id, String firstName, String lastName, int age, String location, int yearsOfExperience, String email, String background) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.location = location;
+        this.yearsOfExperience = yearsOfExperience;
+        this.email = email;
+        this.background = background;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public void setYearsOfExperience(int yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBackground() {
+        return background;
+    }
+
+    public void setBackground(String background) {
+        this.background = background;
+    }
+}
