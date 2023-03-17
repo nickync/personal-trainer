b42fa798bab9ac9699e295b4f9d1bc4eed554d41
nick <lizheng9024@gmail.com>
2023-03-16 22:47:28 -0400

    footer

--- pt-react/src/component/FooterComponent.jsx
+++ pt-react/src/component/FooterComponent.jsx
@@ -1,8 +1,22 @@
 export default function FooterComponent() {
   return (
-    <>  
+    <div className="container-fluid bg-dark bt-gradient">  
         <hr></hr>
-        <div className="text-center text-dark">Copyrights 2023 @ Zen</div>
-    </>
+        <div className="d-flex justify-content-between">
+          <div>
+            <div className="fw-bolder text-light">PERSONAL TRAINER</div>
+            <div className="text-start text-light fst-italic">@ Personal Trainer 2023</div>
+          </div>
+          <div className="text-light text-gradient">
+            <div>Contact: (123) 456-789</div>
+            <div>
+              <div>Address:</div> 
+              <div>270 Park Ave</div>
+              <div>Jonesboro, DC</div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+    </div>
   )
 }
